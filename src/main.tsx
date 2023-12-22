d894e35c22b763744dbde11febc29fb32d6badff
Kenta Yoshii <kenta_yoshii@brown.edu>
2023-12-23 06:18:13 +0900

    cmooon

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import config from "./config/config";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={config.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
