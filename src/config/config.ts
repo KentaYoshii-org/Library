01a43cd197f546e7b3f04341efa232e5b1c1747f
Kenta Yoshii <kenta_yoshii@brown.edu>
2023-12-23 05:43:40 +0900

    check

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,12 +3,14 @@ import { z } from "zod";
 const configSchema = z.object({
   GAPI_KEY: z.string(),
   BUID: z.string(),
+  PUBLIC_URL: z.string(),
 });
 
 const config = configSchema.parse({
   // GAPI_KEY: import.meta.env.VITE_GOOGLE_API_KEY,
   GAPI_KEY: import.meta.env.VITE_GOOGLE_API_KEY_TWO,
   BUID: import.meta.env.VITE_BOOKS_UID,
+  PUBLIC_URL: import.meta.env.VITE_PUBLIC_URL,
 });
 
 export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./app";
+import config from "./config/config";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={config.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
