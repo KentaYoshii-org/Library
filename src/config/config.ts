10f0314652820d6303046037062ac81ce6af4865
Kenta Yoshii <kenta_yoshii@brown.edu>
2023-12-23 10:01:36 +0900

    please work

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,15 +3,13 @@ import { z } from "zod";
 const configSchema = z.object({
   GAPI_KEY: z.string(),
   BUID: z.string(),
-  PUBLIC_URL: z.string(),
   GLB_PATH: z.string(),
 });
 
 const config = configSchema.parse({
   // GAPI_KEY: import.meta.env.VITE_GOOGLE_API_KEY,
-  GAPI_KEY: import.meta.env.VITE_GOOGLE_API_KEY_TWO,
+  GAPI_KEY: import.meta.env.VITE_GOOGLE_API_KEY,
   BUID: import.meta.env.VITE_BOOKS_UID,
-  PUBLIC_URL: import.meta.env.VITE_PUBLIC_URL,
   GLB_PATH: import.meta.env.VITE_NODE_ENV === "prod" ? "/assets/medieval_fantasy_book.glb" : "src/assets/medieval_fantasy_book.glb"
 });
 
